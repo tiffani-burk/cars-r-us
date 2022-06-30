de660412b8d019fbbb687726e76cfac587835a6e
Tiffani Burk <greengurl1088@yahoo.com>
2022-06-30 16:52:24 -0500

    html rendored for paint

--- scripts/paint.js
+++ scripts/paint.js
@@ -3,13 +3,21 @@ import { getPaints } from "./database.js";
 const paintChoices = getPaints
 
 export const paints = () => {
-    return `<h2>Paints</h2>
-    <select id="paint-dropdown">
-    <option value= "0">Silver</option>
-    <option value= "1">Midnight Blue</option>
-    <option value= "2">Firebrick Red</option>
-    <option value= "3">Spring Green</option>
-    </select> 
-    `
+    let html = "<ul>"
+    
+    let listItemArray = paintChoices.map(paintChoice => {
+        return `<h2>Paints</h2>
+        <select id="paint-dropdown">
+        <option value= "0">Silver</option>
+        <option value= "1">Midnight Blue</option>
+        <option value= "2">Firebrick Red</option>
+        <option value= "3">Spring Green</option>
+        </select> 
+        `
+    })
+
+    html + listItemArray.join("")
+    html + "</ul>"
+    return html
 }
 //create an event listener for the paint choices
