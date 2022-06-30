8289e9e84d93046e02642c19b721e3ca104bfa75
Tiffani Burk <greengurl1088@yahoo.com>
2022-06-30 16:49:08 -0500

    html added to tech.js

--- scripts/tech.js
+++ scripts/tech.js
@@ -5,15 +5,19 @@ const techChoices = getTechs()
 export const technologies = () => {
     let html = "<ul>"
 
-    let listItemArray = interiorChoices.map(techChoices) => {
+    let listItemArray = techChoices.map(techChoice => {
+        return `<h2>Technologies</h2>
+        <select id="tech-dropdown">
+        <option value= "0">Basic Package (basic sound system)</option>
+        <option value= "1">Navigation Package (includes integrated navigation controls)</option>
+        <option value= "2">Visibility Package (includes side and reat cameras)"</option>
+        <option value= "3">Ultra Package (includes navigation and visibility packages)</option>
+        </select> `
+    }) 
 
-    return `<h2>Technologies</h2>
-    <select id="tech-dropdown">
-    <option value= "0">Basic Package (basic sound system)</option>
-    <option value= "1">Navigation Package (includes integrated navigation controls)</option>
-    <option value= "2">Visibility Package (includes side and reat cameras)"</option>
-    <option value= "3">Ultra Package (includes navigation and visibility packages)</option>
-    </select> `
+    html + listItemArray.join("")
+    html + "</ul>"
+    return html
 }
 
 //create an event listener for the tech choices
