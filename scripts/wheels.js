5a84d29d8c9dc7b9a2fe113c7847a73f35a92322
Tiffani Burk <greengurl1088@yahoo.com>
2022-06-30 16:54:58 -0500

    rendored html for wheels

--- scripts/wheels.js
+++ scripts/wheels.js
@@ -3,14 +3,22 @@ import { getWheels } from "./database.js";
 const wheelChoices = getWheels
 
 export const wheels = () => {
-    return `<h2>Wheels</h2>
-    <select id="wheels-dropdown">
-    <option value= "0">17-inch Pair Radial</option>
-    <option value= "1">17-inch Pair Radial Black</option>
-    <option value= "2">18-inch Pair Spoke Silver</option>
-    <option value= "3">18-inch Pair Spoke Black</option>
-    </select> 
-    `
+    let html = "<ul>"
+
+    let listItemArray = wheelChoices.map(wheelChoice => {
+        return `<h2>Wheels</h2>
+        <select id="wheels-dropdown">
+        <option value= "0">17-inch Pair Radial</option>
+        <option value= "1">17-inch Pair Radial Black</option>
+        <option value= "2">18-inch Pair Spoke Silver</option>
+        <option value= "3">18-inch Pair Spoke Black</option>
+        </select> 
+        `
+    })
+
+    html + listItemArray.join("")
+    html + "</ul>"
+    return html
 }
 
 //create an event listener for the wheels choices
